b080e0cb26a08e405ce23c43dd43a342ebb18936
maryamkianfar <71686694+maryamkianfar@users.noreply.github.com>
2024-11-11 09:47:29 +0330

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ navbar-light bg-light">
     <div class="container">
         <a class="navbar-brand" href="index.html">Navbar</a>
         <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse"
+
             data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
             aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
